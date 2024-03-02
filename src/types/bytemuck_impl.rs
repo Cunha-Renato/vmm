206a08fb9fd95ece115377c1c853265e76f54fb5
Renato M. Cunha <cunhamrenato@gmail.com>
2024-03-02 09:42:31 -0300

    .

--- src/types/bytemuck_impl.rs
+++ src/types/bytemuck_impl.rs
@@ -1,7 +1,7 @@
 /// This is file aims to integrate the types with the crate [bytemuck](https://crates.io/crates/bytemuck/)
 
 use std::marker;
-use bytemuck::Zeroable;
+use bytemuck::{Pod, Zeroable};
 use super::{VecN, MatN};
 
 unsafe impl<T, const N: usize> Zeroable for VecN<T, N> 
@@ -9,7 +9,7 @@ where
     T: Default + marker::Copy,
     f64: From<T> {}
 
-unsafe impl<T, const N: usize> bytemuck::Pod for VecN<T, N> 
+unsafe impl<T, const N: usize> Pod for VecN<T, N> 
 where
     T: Default + marker::Copy + 'static,
     f64: From<T> {}
@@ -18,7 +18,7 @@ unsafe impl<T, const N: usize> Zeroable for MatN<T, N>
 where 
     T: Default + marker::Copy,
     f64: From<T> {}
-unsafe impl<T, const N: usize> bytemuck::Pod for MatN<T, N>
+unsafe impl<T, const N: usize> Pod for MatN<T, N>
 where
     T: Default + marker::Copy + 'static,
     f64: From<T> {}
